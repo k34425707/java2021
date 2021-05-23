93a77a1b7c8f8d0e1e43b43a33aee98a7c29da48
k34425707 <b102145@gmail.com>
2021-05-23 18:39:19 +0800

    新稱Account類別跟實作寫入帳目

---
+++ src/Account.java
@@ -0,0 +1,69 @@
+public class Account 
+{
+    private int year;
+    private int month;
+    private int date;
+    private int moneySum;
+    private String type;
+    private String description;    
+    
+    public Account(int year,int month,int date,String type,String des,int moneySum)
+    {
+        this.year=year;
+        this.month=month;
+        this.date=date;
+        this.moneySum=moneySum;
+        this.type=type;
+        this.description=des;
+    }
+    
+    public void setYear(int y){
+        this.year=y;
+    }
+
+    public void setMonth(int m){
+        this.month=m;
+    }
+
+    public void setDate(int d){
+        this.date=d;
+    }
+    
+    public void setMoneySum(int MS){
+        this.moneySum=MS;
+    } 
+
+    public void setType (String t){
+        this.type=t;
+    }
+
+    public void setDescription(String des){
+        this.description=des;
+    }
+
+    public int getYear(){
+        return this.year;
+    }
+
+    public int getMonth(){
+        return this.month;
+    }
+
+    public int getDate(){
+        return this.date;
+    }
+    public int getMoneySum() {
+        return this.moneySum;
+    }
+    public String getType()  {
+        return this.type;
+    }
+    public String getDescription() {
+        return this.description;
+    }
+
+    public String writeIntoCsvFile()
+    {
+        return String.format(getYear()+","+getMonth()+","+getDate()+","+getType()+","+getDescription()+","+getMoneySum());
+    }
+}
---
+++ src/BookOperation.java
@@ -0,0 +1,43 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.BufferedWriter;
+import java.io.BufferedReader;
+import java.util.ArrayList;
+public class BookOperation 
+{
+    public void addAccountsIntoCsvFile( ArrayList<Account> input)
+    {
+        String directoryPath;
+        String csvFileName;
+        File directory;
+        File csvFile;
+        BufferedWriter bw;
+        for(Account account:input)
+        {
+            directoryPath=String.format("bookData/book_"+account.getYear());
+            csvFileName=String.format(directoryPath+"/book_"+account.getYear()+"_"+account.getMonth()+".csv");
+            try{
+                directory=new File(directoryPath);
+                csvFile=new File(csvFileName);
+                if(!directory.exists())
+                {
+                    directory.mkdirs();
+                }
+                if(!csvFile.exists())
+                {
+                    csvFile.createNewFile();
+                }
+                bw=new BufferedWriter(new FileWriter(csvFile,true));
+                bw.write(account.writeIntoCsvFile());
+                bw.newLine();
+                bw.close();
+            }
+            catch(IOException e)
+            {
+                System.out.println(e);
+            }
+        }
+    }
+}
