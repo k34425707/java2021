bd7f39206b1ac8c923c8475fc85f2d0e7f491602
k34425707 <b102145@gmail.com>
2021-05-26 21:35:55 +0800

    新增以月為單位查詢功能

--- src/BookOperation.java
+++ src/BookOperation.java
@@ -9,14 +9,19 @@ import java.util.ArrayList;
 
 public class BookOperation 
 {
-    String directoryPath;//目錄路徑
-    String csvFileName;//檔名
-    File directory;
-    File csvFile;
+    ArrayList<Account> accountsList=new ArrayList<>();//查詢帳款時使用的回傳list
+    Account account;//add進accountsList的物件
+    private String baseFilePathString="bookData/book_";
+    private String directoryPath;//目錄路徑
+    private String csvFileName;//檔案路徑
+    private File directory;//
+    private File csvFile;
+    String dataString;//讀取csv檔的每一行儲存點
+    String[] data;//將dataString的資料以逗號分開
     public void addAccountsIntoCsvFile(Account account)
     {
         BufferedWriter bw;
-        directoryPath=String.format("bookData/book_"+account.getYear());
+        directoryPath=String.format(baseFilePathString+account.getYear());
         csvFileName=String.format(directoryPath+"/book_"+account.getYear()+"_"+account.getMonth()+".csv");
         try{
             directory=new File(directoryPath);
@@ -42,27 +47,56 @@ public class BookOperation
 
     public ArrayList<Account> getAccountsFromCsv(int startY,int endY)//以年為單位查詢帳目
     {
-        ArrayList<Account> accountsList=new ArrayList<>();
-        Account account;
         for(int i=startY;i<=endY;i++)
         {
-            directoryPath=String.format("bookData/book_"+i);
+            directoryPath=String.format(baseFilePathString+i);
             directory=new File(directoryPath);
-            File[] fileList=directory.listFiles();
-            String dataString;//讀取csv檔的每一行儲存點
-            String[] data;//將dataString的資料以逗號分開
+            File[] fileList;
+            if(directory.exists())
+            {
+                fileList=directory.listFiles();
+                try
+                {
+                    for(File csv:fileList)//將目錄裡所有資料讀一遍
+                    {
+                        BufferedReader br=new BufferedReader(new FileReader(csv));
+                        while((dataString=br.readLine())!=null)
+                        {
+                            data=dataString.split(",");
+                            account=new Account(Integer.parseInt(data[0]),Integer.parseInt(data[1]),Integer.parseInt(data[2]),data[3],data[4],Integer.parseInt(data[5]));
+                            accountsList.add(account);
+                        } 
+                        
+                    }
+                }
+                catch (IOException e)
+                {
+                    System.out.println(e);
+                }
+            }
+        }
+        return accountsList;
+    }
+    public ArrayList<Account> getAccountsFromCsv(int startY,int startM,int endY,int endM)//以月為單位查詢帳目
+    {
+        LocalDate current=LocalDate.of(startY,startM,2);
+        LocalDate end=LocalDate.of(endY,endM,20);
+        while(end.isAfter(current))
+        {
+            csvFileName=String.format(baseFilePathString+current.getYear()+"/book_"+current.getYear()+"_"+current.getMonthValue()+".csv");
+            current=current.plusMonths(1);
             try
             {
-                for(File csv:fileList)//將目錄裡所有資料讀一遍
+                csvFile=new File(csvFileName);
+                if(csvFile.exists())
                 {
-                    BufferedReader br=new BufferedReader(new FileReader(csv));
+                    BufferedReader br=new BufferedReader(new FileReader(csvFile));
                     while((dataString=br.readLine())!=null)
                     {
                         data=dataString.split(",");
                         account=new Account(Integer.parseInt(data[0]),Integer.parseInt(data[1]),Integer.parseInt(data[2]),data[3],data[4],Integer.parseInt(data[5]));
                         accountsList.add(account);
-                    } 
-                    
+                    }
                 }
             }
             catch (IOException e)
@@ -71,19 +105,5 @@ public class BookOperation
             }
         }
         return accountsList;
-    }
-    public ArrayList<Account> getAccountsFromCsv(int startY,int startM,int endY,int endM)
-    {
-        /*LocalDate startDate= LocalDate.of(startY, startM, 1);
-        System.out.println(startDate);
-        System.out.println(startDate.getYear());
-        System.out.println(startDate.getMonthValue());
-        System.out.println(startDate.getDayOfMonth());
-
-        for(int i=startY;i<=endY)
-        {
-            for(int)
-        }*/
-        return null;
     }
 }
