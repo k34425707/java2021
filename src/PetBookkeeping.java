0c7e8146223c47fe43f276294005a0136727389e
k34425707 <b102145@gmail.com>
2021-06-21 22:13:24 +0800

    完成

--- src/PetBookkeeping.java
+++ src/PetBookkeeping.java
@@ -1,11 +1,11 @@
 import javax.swing.*;
 
-public class test{
+public class PetBookkeeping{
     public static void main(String[] args)
     {
         MainScreen mainscreen = new MainScreen();
         mainscreen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        mainscreen.setSize(500,300);
+        mainscreen.setSize(798,600);
         mainscreen.setVisible(true);
     }
 }
